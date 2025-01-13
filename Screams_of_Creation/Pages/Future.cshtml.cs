6d3a681fd6cbcd65a423e75870178a75429d5c74
Entity-903 <charlesmassie021@gmail.com>
2025-01-13 10:51:03 -0700

    Simple Site completed

---
+++ Screams_of_Creation/Pages/Future.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Screams_of_Creation.Pages
+{
+    public class FutureModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
