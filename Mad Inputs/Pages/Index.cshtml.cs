ab341c5011838d9d19ede349ba5d43c09f7bf5a9
Entity-903 <charlesmassie021@gmail.com>
2025-02-06 03:22:20 -0700

    Mad Inputs functions as intended

--- Mad Inputs/Pages/Index.cshtml.cs
+++ Mad Inputs/Pages/Index.cshtml.cs
@@ -17,21 +17,21 @@ namespace Mad_Inputs.Pages
 
 		}
 
-		public IActionResult OnPost(string Input1, string Input2, string Input3, string Input4, string Input5) 
-		{
-			// Do we initialize Viewbag somewhere, if at all?
-			// If not, how does the program remember passed in values?
-
-			// Read only; cannot set it's value
-			//Request.Form[Input1] = Input1;
-
-			ViewBag.First = Input1;
-			ViewBag.Second = Input2;
-			ViewBag.Third = Input3;
-			ViewBag.Fourth = Input4;
-			ViewBag.Fifth = Input5;
-
-			return RedirectToPage("Story");
-		}
+		//public IActionResult OnPost(string Input1, string Input2, string Input3, string Input4, string Input5) 
+		//{
+		//	// Do we initialize Viewbag somewhere, if at all?
+		//	// If not, how does the program remember passed in values?
+		//
+		//	// Read only; cannot set it's value
+		//	//Request.Form[Input1] = Input1;
+		//
+		//	ViewBag.First = Input1;
+		//	ViewBag.Second = Input2;
+		//	ViewBag.Third = Input3;
+		//	ViewBag.Fourth = Input4;
+		//	ViewBag.Fifth = Input5;
+		//
+		//	return RedirectToPage("Story");
+		//}
 	}
 }
--- Mad Inputs/Pages/Story.cshtml.cs
+++ Mad Inputs/Pages/Story.cshtml.cs
@@ -3,19 +3,34 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Mad_Inputs.Pages
 {
-	public class PrivacyModel : PageModel
+	//[BindProperties]
+	public class StoryModel : PageModel
 	{
-		private readonly ILogger<PrivacyModel> _logger;
+		private readonly ILogger<StoryModel> _logger;
 
-		public PrivacyModel(ILogger<PrivacyModel> logger)
+		public string? First { get; set; }
+		public string? Second { get; set; }
+		public string? Third { get; set; }
+		public string? Fourth { get; set; }
+		public string? Fifth { get; set; }
+
+		public StoryModel(ILogger<StoryModel> logger)
 		{
 			_logger = logger;
 		}
 
-		public void OnGet(string Input1, string Input2, string Input3, string Input4, string Input5)
+		public void OnGet()
 		{
+			
+		}
 
+		public void OnPost(string first, string second, string third, string fourth, string fifth)
+		{
+			First = first;
+			Second = second;
+			Third = third;
+			Fourth = fourth;
+			Fifth = fifth;
 		}
 	}
-
 }
